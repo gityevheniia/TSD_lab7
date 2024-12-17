bb63c8ea7a05ffda90dffe90e3594c209ced8bb3
gityevheniia <bukhalo.yevheniia@lll.kpi.ua>
2024-12-17 18:23:51 +0200

    Реалізовано UnitOfWork для роботи з БД

---
+++ UnitOfWork/EFUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using UserManagement.DAL.EF;
+using UserManagement.DAL.Repositories.Impl;
+using UserManagement.DAL.Repositories.Interfaces;
+
+namespace UserManagement.DAL.UnitOfWork
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        private readonly UserContext _context;
+        private IUserRepository _users;
+
+        public EFUnitOfWork(UserContext context)
+        {
+            _context = context;
+        }
+
+        public IUserRepository Users => _users ??= new UserRepository(_context);
+
+        public async Task SaveAsync() => await _context.SaveChangesAsync();
+
+        public void Dispose() => _context.Dispose();
+    }
+}
---
+++ UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using UserManagement.DAL.Repositories.Interfaces;
+
+namespace UserManagement.DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository Users { get; }
+        Task SaveAsync();
+    }
+}
